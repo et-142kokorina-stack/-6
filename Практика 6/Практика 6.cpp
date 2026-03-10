1c42e1bb7b40edbc9026fa2ffd09edf9476f07dc
et-142kokorina-stack <kristinakokorina78965@gmail.com>
2026-03-10 16:33:40 +0500

    Практика 6.cpp

--- Практика 6/Практика 6.cpp
+++ Практика 6/Практика 6.cpp
@@ -1,20 +1,96 @@
-﻿// Практика 6.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
-
 #include <iostream>
+#include <string>
+using namespace std;
+void inputArray(int*& arr, int& n) {
+    setlocale(LC_ALL, "Russian");
+    cout << "Введите размер массива N: ";
+    cin >> n;
 
-int main()
-{
-    std::cout << "Hello World!\n";
+    arr = new int[n];
+
+    cout << "Введите " << n << " элементов массива:\n";
+    for (int i = 0; i < n; i++) {
+        cout << "arr[" << i << "] = ";
+        cin >> arr[i];
+    }
 }
 
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
+void inputM(int& m, int n) {
+    cout << "Введите количество максимальных чисел M: ";
+    cin >> m;
+}
 
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
+int* findMaxElements(int* arr, int n, int m) {
+  
+    int* tempArr = new int[n];
+    for (int i = 0; i < n; i++) {
+        tempArr[i] = arr[i];
+    }
+
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (tempArr[j] < tempArr[j + 1]) {
+
+                int temp = tempArr[j];
+                tempArr[j] = tempArr[j + 1];
+                tempArr[j + 1] = temp;
+            }
+        }
+    }
+
+    int* result = new int[m];
+    for (int i = 0; i < m; i++) {
+        result[i] = tempArr[i];
+    }
+
+    delete[] tempArr;
+
+    return result;
+}
+
+void printArray(int* arr, int size, const string& name) {
+    cout << name << ": [";
+    for (int i = 0; i < size; i++) {
+        cout << arr[i];
+        if (i < size - 1) {
+            cout << ", ";
+        }
+    }
+    cout << "]" << endl;
+}
+
+int main() {
+    try {
+        int n, m;
+        int* arr = nullptr;
+        int* maxElements = nullptr;
+
+        inputArray(arr, n);
+
+        printArray(arr, n, "Исходный массив");
+
+        inputM(m, n);
+
+        maxElements = findMaxElements(arr, n, m);
+
+        printArray(maxElements, m, "M максимальных чисел");
+
+        delete[] arr;
+        delete[] maxElements;
+
+    }
+    catch (const string& error) {
+        cout << "Ошибка: " << error << endl;
+        return 1;
+    }
+    catch (const bad_alloc& e) {
+        cout << "Ошибка выделения памяти" << endl;
+        return 1;
+    }
+    catch (...) {
+        cout << "Неизвестная ошибка" << endl;
+        return 1;
+    }
+
+    return 0;
+}

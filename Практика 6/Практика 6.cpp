3c445f9b689c3a2cecadbd448783f0bfff040c52
et-142kokorina-stack <kristinakokorina78965@gmail.com>
2026-03-13 00:12:57 +0500

    Практика 6.cpp

--- Практика 6/Практика 6.cpp
+++ Практика 6/Практика 6.cpp
@@ -1,106 +1,52 @@
 #include <iostream>
-#include <string>
 using namespace std;
+int main()
+{
+    setlocale(LC_ALL,"");
+    const int MAX_SIZE = 1000;
+    int arr[MAX_SIZE] = { 0 };
+    int maxArr[MAX_SIZE] = { 0 };
+    int N, M;
 
-int partition(int* arr, int low, int high) {
-    int pivot = arr[high];
-    int i = (low - 1);
+    cout << "Введите N и M: ";
+    cin >> N >> M;
 
-    for (int j = low; j <= high - 1; j++) {
-        if (arr[j] >= pivot) {
-            i++;
-            swap(arr[i], arr[j]);
+    for (int i = 0; i < N; i++) {
+        arr[i] = rand() % 1000;
+    }
+
+    cout << "Исходный массив: ";
+    for (int i = 0; i < N; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    int temp;
+    for (int i = 0; i < N - 1; i++) {
+        for (int j = 0; j < N - i - 1; j++) {
+            if (arr[j] < arr[j + 1]) {
+                temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
         }
     }
-    swap(arr[i + 1], arr[high]);
-    return (i + 1);
-}
 
-void quickSort(int* arr, int low, int high) {
-    if (low < high) {
-        int pi = partition(arr, low, high);
-
-        quickSort(arr, low, pi - 1);
-        quickSort(arr, pi + 1, high);
+    cout << "Отсортированный массив: ";
+    for (int i = 0; i < N; i++) {
+        cout << arr[i] << " ";
     }
-}
+    cout << endl;
 
-void inputArray(int*& arr, int& n) {
-    setlocale(LC_ALL, "Russian");
-    cout << "Введите размер массива N: ";
-    cin >> n;
-
-    arr = new int[n];
-
-    cout << "Введите " << n << " элементов массива:\n";
-    for (int i = 0; i < n; i++) {
-        cout << "arr[" << i << "] = ";
-        cin >> arr[i];
+    for (int i = 0; i < M; i++) {
+        maxArr[i] = arr[i];
     }
-}
-
-void inputM(int& m, int n) {
-    cout << "Введите количество максимальных чисел M: ";
-    cin >> m;
-}
-
-int* findMaxElements(int* arr, int n, int m) {
-    int* tempArr = new int[n];
-    for (int i = 0; i < n; i++) {
-        tempArr[i] = arr[i];
+    cout << "M максимальных чисел: ";
+    for (int i = 0; i < M; i++) {
+        cout << maxArr[i];
+        if (i < M - 1) cout << ", ";
     }
-    
-    quickSort(tempArr, 0, n - 1);
-
-    int* result = new int[m];
-    for (int i = 0; i < m; i++) {
-        result[i] = tempArr[i];
-    }
-
-    return result;
-}
-
-void printArray(int* arr, int size, const string& name) {
-    cout << name << ": [";
-    for (int i = 0; i < size; i++) {
-        cout << arr[i];
-        if (i < size - 1) {
-            cout << ", ";
-        }
-    }
-    cout << "]" << endl;
-}
-
-int main() {
-    try {
-        int n, m;
-        int* arr = nullptr;
-        int* maxElements = nullptr;
-
-        inputArray(arr, n);
-        printArray(arr, n, "Исходный массив");
-
-        inputM(m, n);
-        maxElements = findMaxElements(arr, n, m);
-
-        printArray(maxElements, m, "M максимальных чисел");
-
-        delete[] arr;
-        delete[] maxElements;
-
-    }
-    catch (const string& error) {
-        cout << "Ошибка: " << error << endl;
-        return 1;
-    }
-    catch (const bad_alloc& e) {
-        cout << "Ошибка выделения памяти" << endl;
-        return 1;
-    }
-    catch (...) {
-        cout << "Неизвестная ошибка" << endl;
-        return 1;
-    }
+    cout << endl;
 
     return 0;
 }
